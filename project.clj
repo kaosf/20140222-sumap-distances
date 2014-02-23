4a78f0890db3e32b034fea6d2afed5437b943ac2
ka <ka.kaosf@gmail.com>
2014-02-23 14:32:55 +0900

    impelement retrieve-lat-lon

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clojure-csv "2.0.0-alpha2"]]
+                 [clojure-csv "2.0.0-alpha2"]
+                 [clj-http "0.7.9"]]
   :main ^:skip-aot sumap-distances.core
   :target-path "target/%s"
   :profiles {
--- src/sumap_distances/core.clj
+++ src/sumap_distances/core.clj
@@ -1,5 +1,28 @@
 (ns sumap-distances.core
+  (:import [java.net URLEncoder])
+  (:require [clojure-csv.core :refer (parse-csv)]
+            [clj-http.client :as http]
+            [clojure.xml :as xml]
+            [clojure.string :refer (split)])
   (:gen-class))
+
+(def addresses
+  (->
+    (slurp "inputdata.csv")
+    parse-csv
+    ;rest
+    vec))
+
+(defn retrieve-lat-lon [address]
+  (let [key ""
+        pflg "2"
+        outf "TSV"
+        datum "WGS84"
+        frewd (URLEncoder/encode address)
+        url (str "http://test.cgi.e-map.ne.jp/cgi/selectaddr.cgi?key=" key "&enc=UTF8&pflg=" pflg "&mclv=3&sep=1&outf=" outf "&datum=" datum "&frewd=" frewd)
+        body ((http/get url) :body)
+        [_ _ _ _ _ lat lon] (split ((split body #"\r\n") 1) #"\t")]
+    [lat lon]))
 
 (defn -main
   "I don't do a whole lot ... yet."
